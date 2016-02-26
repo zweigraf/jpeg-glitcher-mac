2fdbf2aecfe9bc63f07e78b538ac81c4b8af2457
Luis Reisewitz <reisewitz@me.com>
2016-02-26 22:38:05 +0100

    Initial Commit

---
+++ JPEGGlitcher/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  JPEGGlitcher
+//
+//  Created by Luis Reisewitz on 26.02.16.
+//  Copyright © 2016 ZweiGraf. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ JPEGGlitcher/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JPEGGlitcher
+//
+//  Created by Luis Reisewitz on 26.02.16.
+//  Copyright © 2016 ZweiGraf. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

60e24c2b6c8c1eb01513c49e27643a43ff85bc3e
Luis Reisewitz <reisewitz@me.com>
2016-02-27 15:43:51 +0100

    randomly modify one byte of the image before converting back to NSData and displaying it

--- JPEGGlitcher/ViewController.swift
+++ JPEGGlitcher/ViewController.swift
@@ -40,13 +40,33 @@ class ViewController: NSViewController {
                     return
                 }
                 
-                guard let image = NSImage(data: data) else {
+                let newData = self.glitchData(data);
+                
+                guard let image = NSImage(data: newData) else {
+                    print("could not create image from glitched data")
                     return
                 }
                 
                 self.imageView.image = image;
+                
+                
             }
         }
+    }
+    
+    func glitchData(data : NSData) -> NSData {
+        let count = data.length / sizeof(UInt8);
+        var bytes = [UInt8](count: count, repeatedValue : 0);
+        
+        data.getBytes(&bytes, length: data.length);
+        
+        let randomIndex = Int(drand48() * Double(count));
+        let randomValue = UInt8(drand48() * Double(UInt8.max));
+        
+        bytes[randomIndex] = randomValue;
+        
+        let newData = NSData(bytes: &bytes, length: data.length);
+        return newData;
     }
 
 }
